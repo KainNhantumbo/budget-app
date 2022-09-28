c0290caad7cd73e88d2cb09ba2b88749303ededa
Kain Nhantumbo <nhantumbok@gmail.com>
2022-09-28 22:11:02 +0200

    docs: added CopyrightNotice file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import './css/main.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ThemeContext from './context/ThemeContext';
 import App from './App';
-import './css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>

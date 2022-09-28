0ea8346491306e2ee0a796b0e3b0c4f4f7ed3729
Kain Nhantumbo <nhantumbok@gmail.com>
2022-09-28 17:38:12 +0200

    feat: added dark and light color themes.

---
+++ src/themes/colors.ts
@@ -0,0 +1,25 @@
+import { ThemeType } from "../types/themeType";
+
+export const lightColors: ThemeType = {
+	primary: '134, 72, 121',
+	secondary: '188, 37, 81',
+	font: '95, 99, 104',
+	accent: '12, 3, 13',
+	neutral: '255, 255, 255',
+	background: '245, 240, 246',
+	foreground: '255, 255, 255',
+	alternative_a: '141, 149, 122',
+	alternative_b: '104, 93, 102',
+};
+
+export const darkColors: ThemeType = {
+	primary: '135, 85, 111',
+	secondary: '59, 82, 73',
+	font: '178, 177, 185',
+	accent: '25, 25, 28',
+	neutral: '240, 240,240',
+	background: '32, 33, 36',
+	foreground: '41, 42, 46',
+	alternative_a: '141, 149, 122',
+	alternative_b: '74, 63, 53',
+};
